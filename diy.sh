b0c22b9f54ea474e0b48d1d52adbbf24fcb3f381
miyafung <752875397@qq.com>
2020-05-21 13:35:25 +0800

    Update diy.sh
    默认地址修改192.168.1.1的默认地址

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.18.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' openwrt/package/base-files/files/bin/config_generate
 # Install ServerChan
 git clone https://github.com/tty228/luci-app-serverchan openwrt/package/lean/luci-app-serverchan
 # Install passwall
